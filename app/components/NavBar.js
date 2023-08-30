b8347f2a16ac05d7990bee79b908d8892cfd9522
emjayholla <maggie.hollander@gmail.com>
2023-08-30 16:52:23 -0500

    Create NavBar.js
    basic navbar set up

---
+++ app/components/NavBar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
